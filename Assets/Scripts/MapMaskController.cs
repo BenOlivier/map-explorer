4d32d82854fe3a514254f1818c7d7fd5c510f8c5
BenOlivier <dbeolivier@gmail.com>
2023-10-14 11:48:22 +0100

    Create procedural mask

--- Assets/Scripts/MapMaskController.cs
+++ Assets/Scripts/MapMaskController.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class MapMaskController : MonoBehaviour
 {
-    public Texture2D MaskTexture;
     public float MaskScale;
+    public float MaskLength;
+    public float MaskWidth;
+    public float MaskRadius;
     public float MaskRotation;
 
     private void Update()
@@ -18,18 +20,20 @@ public class MapMaskController : MonoBehaviour
 
     public void SetValues()
     {
-        Shader.SetGlobalTexture("_MaskTexture", MaskTexture);
         Shader.SetGlobalFloat("_MaskScale", MaskScale);
+        Shader.SetGlobalFloat("_MaskLength", MaskLength / MaskScale);
+        Shader.SetGlobalFloat("_MaskWidth", MaskWidth / MaskScale);
+        Shader.SetGlobalFloat("_MaskRadius", MaskRadius / MaskScale);
         Shader.SetGlobalFloat("_MaskRotation", MaskRotation);
         Shader.SetGlobalVector("_MaskPosition", transform.position);
     }
 
     private void OnDrawGizmos()
     {
-        var v1 = transform.position + Quaternion.Euler(0, MaskRotation, 0) * new Vector3(-MaskScale / 2, 0, -MaskScale / 2);
-        var v2 = transform.position + Quaternion.Euler(0, MaskRotation, 0) * new Vector3(-MaskScale / 2, 0, +MaskScale / 2);
-        var v3 = transform.position + Quaternion.Euler(0, MaskRotation, 0) * new Vector3(+MaskScale / 2, 0, +MaskScale / 2);
-        var v4 = transform.position + Quaternion.Euler(0, MaskRotation, 0) * new Vector3(+MaskScale / 2, 0, -MaskScale / 2);
+        var v1 = transform.position + Quaternion.Euler(0, MaskRotation, 0) * new Vector3(-MaskWidth / 2, 0, -MaskLength / 2);
+        var v2 = transform.position + Quaternion.Euler(0, MaskRotation, 0) * new Vector3(-MaskWidth / 2, 0, +MaskLength / 2);
+        var v3 = transform.position + Quaternion.Euler(0, MaskRotation, 0) * new Vector3(+MaskWidth / 2, 0, +MaskLength / 2);
+        var v4 = transform.position + Quaternion.Euler(0, MaskRotation, 0) * new Vector3(+MaskWidth / 2, 0, -MaskLength / 2);
 
         Gizmos.DrawLine(v1, v2);
         Gizmos.DrawLine(v2, v3);
