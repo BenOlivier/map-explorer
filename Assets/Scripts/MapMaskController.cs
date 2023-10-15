eb7cfc5e3aaf56abf7aceec49ab56dd9ed9e59bc
BenOlivier <dbeolivier@gmail.com>
2023-10-15 12:32:14 +0100

    Covert to SDF based mask

--- Assets/Scripts/MapMaskController.cs
+++ Assets/Scripts/MapMaskController.cs
@@ -6,7 +6,7 @@ public class MapMaskController : MonoBehaviour
     public float MaskLength;
     public float MaskWidth;
     public float MaskRadius;
-    public float MaskRotation;
+    public float FadeWidth;
 
     private void Update()
     {
@@ -24,16 +24,15 @@ public class MapMaskController : MonoBehaviour
         Shader.SetGlobalFloat("_MaskLength", MaskLength / MaskScale);
         Shader.SetGlobalFloat("_MaskWidth", MaskWidth / MaskScale);
         Shader.SetGlobalFloat("_MaskRadius", MaskRadius / MaskScale);
-        Shader.SetGlobalFloat("_MaskRotation", MaskRotation);
-        Shader.SetGlobalVector("_MaskPosition", transform.position);
+        Shader.SetGlobalFloat("_FadeWidth", FadeWidth);
     }
 
     private void OnDrawGizmos()
     {
-        var v1 = transform.position + Quaternion.Euler(0, MaskRotation, 0) * new Vector3(-MaskWidth / 2, 0, -MaskLength / 2);
-        var v2 = transform.position + Quaternion.Euler(0, MaskRotation, 0) * new Vector3(-MaskWidth / 2, 0, +MaskLength / 2);
-        var v3 = transform.position + Quaternion.Euler(0, MaskRotation, 0) * new Vector3(+MaskWidth / 2, 0, +MaskLength / 2);
-        var v4 = transform.position + Quaternion.Euler(0, MaskRotation, 0) * new Vector3(+MaskWidth / 2, 0, -MaskLength / 2);
+        var v1 = transform.position + new Vector3(-MaskWidth / 2 - FadeWidth * MaskScale, 0, -MaskLength / 2 - FadeWidth * MaskScale);
+        var v2 = transform.position + new Vector3(-MaskWidth / 2 - FadeWidth * MaskScale, 0, +MaskLength / 2 + FadeWidth * MaskScale);
+        var v3 = transform.position + new Vector3(+MaskWidth / 2 + FadeWidth * MaskScale, 0, +MaskLength / 2 + FadeWidth * MaskScale);
+        var v4 = transform.position + new Vector3(+MaskWidth / 2 + FadeWidth * MaskScale, 0, -MaskLength / 2 - FadeWidth * MaskScale);
 
         Gizmos.DrawLine(v1, v2);
         Gizmos.DrawLine(v2, v3);
